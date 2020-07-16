60761cac5c9eec2bdc66047fd868e5066083b98d
Joel Brown <joelbrown@Joels-MacBook-Pro.local>
2020-07-16 09:26:01 -0400

    storing new product on the database

--- store/actions/products.js
+++ store/actions/products.js
@@ -22,14 +22,34 @@ export const updateProduct = (pid, title, description, imageUrl) => {
     }
 }
 export const createProduct = (title, description, imageUrl, price) => { 
-    console.log('inside the actions file')
-    return {
-        type: CREATE_PRODUCT,
-        productData: {
-            title,
-            description,
-            imageUrl,
-            price
-        }
+    return async dispatch => {
+        const url = "https://shopping-app-9a925.firebaseio.com/products.json";
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title, 
+                description, 
+                imageUrl, 
+                price
+            })
+        });
+
+        const respData = await response.json()
+
+        console.log('response data: ', respData);
+
+        dispatch ({
+            type: CREATE_PRODUCT,
+            productData: {
+                id: respData.name
+                title,
+                description,
+                imageUrl,
+                price
+            }
+        })
     }
 }
